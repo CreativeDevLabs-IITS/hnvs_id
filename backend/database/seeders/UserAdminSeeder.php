72f87e5e6fa19c643cd94f110eb51b1fff31c940
Jae Voy Seloterio <sjaevoy@gmail.com>
2025-07-22 14:43:14 +0800

    admin-seeder

---
+++ backend/database/seeders/UserAdminSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class UserAdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::created([
+            'firstname' => 'Jaevoy',
+            'lastname' => 'Seloterio',
+            'contact' => '0000000000',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('admin@123!'),
+            'role' => 0,
+        ]);
+    }
+}
