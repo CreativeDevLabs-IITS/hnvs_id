d1d9d3ec52d5e9fb426e654441dda79e7d1ed675
jaevoy2 <sjaevoy2@gmail.com>
2025-09-27 22:23:26 +0800

    Update config.php

--- frontend/partials/config.php
+++ frontend/partials/config.php
@@ -1,6 +1,5 @@
 <?php 
 
-    define('APP_URL', 'https://hnvs-id-be.creativedevlabs.com');
-    define('FRONTEND_URL', 'https://hnvs-id.creativedevlabs.com')
-
+    define('APP_URL', 'http://hnvs_backend.test');
+    define('FRONTEND_URL', 'http://hnvs.system.test')
 ?>
