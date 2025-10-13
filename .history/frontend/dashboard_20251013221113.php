1e0267d8a69da0fc5447c9e5194716044fc3d7f8
Jae Voy Seloterio <sjaevoy@gmail.com>
2025-10-13 22:49:39 +0800

    dashboard-total-id

--- .history/frontend/dashboard_20251013221113.php
+++ .history/frontend/dashboard_20251013221113.php
@@ -11,32 +11,23 @@
             <div class="py-3 pe-3 ps-5">
                 <div class="fs-4">Dashboard</div>
                 <div class="d-flex flex-wrap gap-4 mt-5">
-                    <div class="ps-4 pe-5 py-4 bg-white rounded-4 shadow" id="id_percentage" style="width: 300px; display: none">
+                    <div class="ps-4 pe-5 py-4 bg-white rounded-4 shadow" id="id_percentage" style="width: 300px;">
                         <div style="font-size: 18px;">Total ID Percentage</div>
                         <div class="fw-bold fs-1 mt-3 d-flex align-items-center gap-2">
                             <div class="progress-ring" id="circle-border">
-                                <svg width="120" height="120">
-                                    <circle class="bg" cx="60" cy="60" r="45"></circle>
-                                    <circle class="progress" cx="60" cy="60" r="45"></circle>
+                                <svg>
+                                    <circle class="bg" cx="50" cy="50" r="45"></circle>
+                                    <circle class="progress" cx="50" cy="50" r="45"></circle>
                                 </svg>
-                                <div id="percent" style="color: #3498DB"></div>
+                                <div id="percent"></div>
                             </div>
-                            <div class="d-flex flex-column">
-                                <div class="d-flex align-items-center gap-1">
-                                    <div style="background-color: #3498DB; width: 15px; height: 15px"></div>
-                                    <div id="totalId" style="font-size: 15px; font-weight: 400"></div>
-                                </div>
-                                <div class="d-flex align-items-center gap-1">
-                                    <div style="background-color: #F54927; width: 15px; height: 15px"></div>
-                                    <div id="noIdTotal" style="font-size: 15px; font-weight: 400"></div>
-                                </div>
-                            </div>
+                            <div></div>
                         </div>
                     </div>
                     <div class="ps-4 pe-5 py-4 bg-white rounded-4 shadow" style="width: 300px;">
                         <div style="font-size: 18px;">Students</div>
                         <div class="fw-bold fs-1 mt-3 d-flex align-items-center gap-2">
-                            <svg class="me-2" xmlns="http://www.w3.org/2000/svg"  width="32"  height="32"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-school"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M22 9l-10 -4l-10 4l10 4l10 -4v6" /><path d="M6 10.6v5.4a6 3 0 0 0 12 0v-5.4" /></svg>                        
+                            <svg class="me-2" xmlns="http://www.w3.org/2000/svg"  width="30"  height="30"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-school"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M22 9l-10 -4l-10 4l10 4l10 -4v6" /><path d="M6 10.6v5.4a6 3 0 0 0 12 0v-5.4" /></svg>                        
                             <div id="student_num"></div>
                         </div>
                     </div>
@@ -44,7 +35,7 @@
                     <div class="ps-4 pe-5 py-4 bg-white rounded-4 shadow" id="teacherNumCon" style="width: 300px; display: none">
                         <div style="font-size: 18px;">Teachers</div>
                         <div class="fw-bold fs-1 mt-3 d-flex align-items-center gap-2">
-                            <svg  xmlns="http://www.w3.org/2000/svg"  width="30"  height="30"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-users"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 7m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0" /><path d="M3 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" /><path d="M16 3.13a4 4 0 0 1 0 7.75" /><path d="M21 21v-2a4 4 0 0 0 -3 -3.85" /></svg>
+                            <svg  xmlns="http://www.w3.org/2000/svg"  width="28"  height="28"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-users"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 7m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0" /><path d="M3 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" /><path d="M16 3.13a4 4 0 0 1 0 7.75" /><path d="M21 21v-2a4 4 0 0 0 -3 -3.85" /></svg>
                             <div id="teacher_num"></div>
                         </div>
                     </div>
@@ -93,8 +84,6 @@
         .then(res => res.json())
         .then(data => {
             document.getElementById('student_num').textContent = data.students;
-            document.getElementById('totalId').textContent = `With ID: ${data.idcount}`;
-            document.getElementById('noIdTotal').textContent = `No ID: ${data.noIdCount}`;
             getPercentage(data.idcount, data.students);
         });
 
@@ -110,7 +99,6 @@
             console.log(data);
             if(data.user == 1 || data.user == 0) {
                 document.getElementById('teacherNumCon').style.display = 'block';
-                document.getElementById('id_percentage').style.display = 'block';
                 document.getElementById('teacher_num').textContent = data.teachers;
             }
         });
@@ -119,12 +107,12 @@
 
 
     function getPercentage(idCount, studentCount) {
-        const circle = document.querySelector('.progress');
+        const circle = document.querySelector('.progress-ring__progress');
+        const radius = circle.r.baseVal.value;
         let percent = document.getElementById('percent');
         const percentage = Math.round(idCount / studentCount * 100);
 
         const color = '#3498DB';
-        const radius = 45;
         const circumference = 2 * Math.PI * radius;
         const offset = circumference - (percentage / 100) *circumference;
         
--- .history/frontend/dashboard_20251013224030.php
+++ .history/frontend/dashboard_20251013224030.php
@@ -11,7 +11,7 @@
             <div class="py-3 pe-3 ps-5">
                 <div class="fs-4">Dashboard</div>
                 <div class="d-flex flex-wrap gap-4 mt-5">
-                    <div class="ps-4 pe-5 py-4 bg-white rounded-4 shadow" id="id_percentage" style="width: 300px; display: none">
+                    <div class="ps-4 pe-5 py-4 bg-white rounded-4 shadow" id="id_percentage" style="width: 300px;">
                         <div style="font-size: 18px;">Total ID Percentage</div>
                         <div class="fw-bold fs-1 mt-3 d-flex align-items-center gap-2">
                             <div class="progress-ring" id="circle-border">
@@ -19,15 +19,15 @@
                                     <circle class="bg" cx="60" cy="60" r="45"></circle>
                                     <circle class="progress" cx="60" cy="60" r="45"></circle>
                                 </svg>
-                                <div id="percent" style="color: #3498DB"></div>
+                                <div id="percent"></div>
                             </div>
                             <div class="d-flex flex-column">
                                 <div class="d-flex align-items-center gap-1">
-                                    <div style="background-color: #3498DB; width: 15px; height: 15px"></div>
+                                    <div style="background-color: #3498DB; width: 20px; height: 20px"></div>
                                     <div id="totalId" style="font-size: 15px; font-weight: 400"></div>
                                 </div>
                                 <div class="d-flex align-items-center gap-1">
-                                    <div style="background-color: #F54927; width: 15px; height: 15px"></div>
+                                    <div style="background-color: #F54927; width: 20px; height: 20px"></div>
                                     <div id="noIdTotal" style="font-size: 15px; font-weight: 400"></div>
                                 </div>
                             </div>
@@ -36,7 +36,7 @@
                     <div class="ps-4 pe-5 py-4 bg-white rounded-4 shadow" style="width: 300px;">
                         <div style="font-size: 18px;">Students</div>
                         <div class="fw-bold fs-1 mt-3 d-flex align-items-center gap-2">
-                            <svg class="me-2" xmlns="http://www.w3.org/2000/svg"  width="32"  height="32"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-school"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M22 9l-10 -4l-10 4l10 4l10 -4v6" /><path d="M6 10.6v5.4a6 3 0 0 0 12 0v-5.4" /></svg>                        
+                            <svg class="me-2" xmlns="http://www.w3.org/2000/svg"  width="30"  height="30"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-school"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M22 9l-10 -4l-10 4l10 4l10 -4v6" /><path d="M6 10.6v5.4a6 3 0 0 0 12 0v-5.4" /></svg>                        
                             <div id="student_num"></div>
                         </div>
                     </div>
@@ -44,7 +44,7 @@
                     <div class="ps-4 pe-5 py-4 bg-white rounded-4 shadow" id="teacherNumCon" style="width: 300px; display: none">
                         <div style="font-size: 18px;">Teachers</div>
                         <div class="fw-bold fs-1 mt-3 d-flex align-items-center gap-2">
-                            <svg  xmlns="http://www.w3.org/2000/svg"  width="30"  height="30"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-users"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 7m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0" /><path d="M3 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" /><path d="M16 3.13a4 4 0 0 1 0 7.75" /><path d="M21 21v-2a4 4 0 0 0 -3 -3.85" /></svg>
+                            <svg  xmlns="http://www.w3.org/2000/svg"  width="28"  height="28"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-users"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 7m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0" /><path d="M3 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" /><path d="M16 3.13a4 4 0 0 1 0 7.75" /><path d="M21 21v-2a4 4 0 0 0 -3 -3.85" /></svg>
                             <div id="teacher_num"></div>
                         </div>
                     </div>
@@ -110,7 +110,6 @@
             console.log(data);
             if(data.user == 1 || data.user == 0) {
                 document.getElementById('teacherNumCon').style.display = 'block';
-                document.getElementById('id_percentage').style.display = 'block';
                 document.getElementById('teacher_num').textContent = data.teachers;
             }
         });
